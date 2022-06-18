2703eec2b0b2a7a348ae193e4f50b5e5e4466610
José Freites <jlfu2004@gmail.com>
2022-06-18 16:21:18 -0300

    ADD: comments | FIXED: ortographics errors h tags

--- main.js
+++ main.js
@@ -1,3 +1,12 @@
+/**
+ * TODO: This exercise shows how to use:
+ * @param prompt() to get a user input,
+ * @param console.log() and
+ * @param console.table() to show the result into de console browser.
+ *
+ * * Also, shows how to create an object and define object properties.
+ */
+
 // Defining and Loading variables values from user using prompt()
 let nname = prompt("Enter a name: ", "José");
 let nage = prompt("Enter your age: ", "54");
